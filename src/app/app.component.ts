7ef1aa50ce257ec1982e2705488b29f6bec57615
Sean Graves <seankgraves@gmail.com>
2016-11-26 11:43:44 -0500

    Make it possible to add a day.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,27 +2,46 @@ import { Component, OnChanges, OnInit } from '@angular/core';
 import {DayData} from './day-data';
 
 
+const createDate = (dayAdjust: number): Date => {
+  const date = new Date();
+
+  date.setDate(date.getDate() + dayAdjust);
+
+  return date;
+
+}
+
 const dayData: DayData[] = [
   {
-    date: 25,
+    date: createDate(0),
     cals: 1765,
     weight: 191.2
   },
   {
-    date: 24,
+    date: createDate(-1),
     cals: 1757,
     weight: 192.1
   },
   {
-    date: 23,
-    cals: 1753,
-    weight: 191.7
+    date: createDate(-2),
+    cals: 1757,
+    weight: 192.1
   },
   {
-    date: 22,
-    cals: 1776,
-    weight: 192.8
-  }
+    date: createDate(-3),
+    cals: 1757,
+    weight: 192.1
+  },
+  // {
+  //   date: 23,
+  //   cals: 1753,
+  //   weight: 191.7
+  // },
+  // {
+  //   date: 22,
+  //   cals: 1776,
+  //   weight: 192.8
+  // }
 
 ];
 
@@ -39,13 +58,30 @@ export class AppComponent implements OnChanges, OnInit {
   calsAverage: number;
   weightAverage: number;
 
+  // nextDateToAdd: Date;
 
 
   ngOnInit() {
+
+    // const lowestDate = this.days.reduce((prev, curr, i) => prev.date > curr.date ? prev : curr).date;
+
+    // lowestDate.setDate(lowestDate.getDate() - 1);
+    // this.nextDateToAdd = lowestDate;
+
+
     this.calcAverages();
   }
 
   ngOnChanges() {
+    this.calcAverages();
+  }
+
+
+  addDay() {
+    console.log('adding day');
+    let{date, cals, weight}  = this.days[this.days.length - 1];
+    date.setDate(date.getDate() - 1);
+    this.days.push({date, cals, weight});
     this.calcAverages();
   }
 
@@ -67,6 +103,7 @@ export class AppComponent implements OnChanges, OnInit {
 
     this.calcAverages();
   }
+
 
 
 
